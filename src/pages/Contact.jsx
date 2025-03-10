782e6231c836ec5ac077bf1434b0ce0e082605b8
maxhandley97 <maxhandley97@gmail.com>
2025-03-10 16:37:08 +1100

    spelling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,8 +16,8 @@ const Contact = () => {
             <strong>Mail:</strong> <a href="mailto:evakluge.praxis@yahoo.com" className="contact-link">evakluge.praxis@yahoo.com</a><br />
             <strong>Telefon:</strong> <a href="tel:+491745399457" className="contact-link">0174 53 99 457</a>
             <br /> <strong>Praxiszeiten:</strong><br />Montag <br /> Dienstag <br /> Donnerstag 13-18 Uhr <br /> bei Bedarf auch andere Zeiten
-            <br /> <strong>Termine:</strong> nur nach Vereinbarung danach neue Zeile
-Nutzen Sie gerne die Möglichkeit, mich anzurufen, mir eine Mail zu schreiben oder das Kontaktformular. Offene Fragen und Termine können wir auf diese Weise direkt klären.
+            <br /> <strong>Termine:</strong> nur nach Vereinbarung <br />
+Nutzen Sie gerne die Möglichkeit, mich anzurufen, mir eine Mail zu schreiben oder das Kontaktformular. <br />Offene Fragen und Termine können wir auf diese Weise direkt klären.
 
           </p>
         </div>
